a154981930a750bebe70bd0c7e827e3e6d60b755
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-03-27 21:01:19 +0530

    Thunder client setup done for server request

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("testing....");
+//import express from "express" this statement will not work use the state below of commonJS
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "received", data: req.body });
+});
+app.listen(5173, () => {
+  console.log("server running....");
+});
