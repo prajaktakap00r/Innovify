f623e9b9475c13bd0898f758fb8be50f7a35f24d
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-03-27 21:28:59 +0530

    checking new features

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+//import * as dotenv from "dotenv";
+const dotenv = require("dotenv");
+dotenv.config();
 //import express from "express" this statement will not work use the state below of commonJS
 const express = require("express");
 const app = express();
 
 const morgan = require("morgan");
 
-app.use(express.json());
+fetch("https://www.course-api.com/react-useReducer-cart-project")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -15,6 +24,14 @@ app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "received", data: req.body });
 });
-app.listen(5173, () => {
-  console.log("server running....");
+
+const port = process.env.PORT || 5173;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}....`);
 });
+
+//New Features
+//-Fetch API
+//-Global await
+//-watch mode  same as nodemon
