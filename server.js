0d289306f2af98ec2aeee9114a6074f94714abf6
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-03-27 21:09:19 +0530

    env setup

--- server.js
+++ server.js
@@ -2,7 +2,12 @@
 const express = require("express");
 const app = express();
 
+const morgan = require("morgan");
+
 app.use(express.json());
+
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
