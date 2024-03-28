080e899198cf88ffd93badfdb063855e8fafc411
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-03-28 21:58:00 +0530

    CRUD operations done

--- server.js
+++ server.js
@@ -71,6 +71,20 @@ app.patch("/api/v1/jobs/:id", (req, res) => {
   job.position = position;
   res.status(200).json({ msg: "job modified", job });
 });
+
+//DELETE JOB
+app.delete("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+
+  res.status(200).json({ msg: "job deleted" });
+});
+
 const port = process.env.PORT || 5173;
 
 app.listen(port, () => {
