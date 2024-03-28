9ef96fc48a8c3a6642e788a7deeae6b2aed430a4
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-03-28 20:51:06 +0530

    edit job

--- server.js
+++ server.js
@@ -27,11 +27,13 @@ app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "received", data: req.body });
 });
-//get all jobs
+
+//GET ALL JOBS
 app.get("/api/v1/jobs", (req, res) => {
   res.status(200).json({ jobs });
 });
-//create jobs
+
+//CREATE JOBS
 app.post("/api/v1/jobs", (req, res) => {
   const { company, position } = req.body;
   if (!company || !position) {
@@ -43,6 +45,7 @@ app.post("/api/v1/jobs", (req, res) => {
   jobs.push(job);
   res.status(201).json({ job });
 });
+
 //GET SINGLE JOB
 app.get("/api/v1/jobs/:id", (req, res) => {
   const { id } = req.params;
@@ -51,6 +54,22 @@ app.get("/api/v1/jobs/:id", (req, res) => {
     return res.status(404).json({ msg: `NO Job with id ${id}` });
   }
   res.status(200).json({ job });
+});
+
+//EDIT JOBS
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "Please enter company and position" });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `No job with id ${id}` });
+  }
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: "job modified", job });
 });
 const port = process.env.PORT || 5173;
 
