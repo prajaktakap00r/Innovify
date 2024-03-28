c4ce1b2e7fceb1ea6f3796f42f88ceef2c1943d6
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-03-28 19:33:03 +0530

    create job done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express");
 
 const app = express();
 const morgan = require("morgan");
-//simple import of nanoid is giving error
+//nanoid is giving error
 
 const { v4: uuidv4 } = require("uuid");
 let jobs = [
@@ -26,6 +26,22 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "received", data: req.body });
+});
+//get all jobs
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
+});
+//create jobs
+app.post("/api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  }
+  const id = uuidv4(10);
+
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(200).json({ job });
 });
 
 const port = process.env.PORT || 5173;
