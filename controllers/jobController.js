369ccb278775a695750ad9d4557a71adfda48fb0
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-03-29 17:44:17 +0530

    controllers setup

---
+++ controllers/jobController.js
@@ -0,0 +1,57 @@
+const { v4: uuidv4 } = require("uuid");
+let jobs = [
+  { id: uuidv4(), company: "apple", position: "front-end" },
+  { id: uuidv4(), company: "google", position: "back-end" },
+];
+export const getAllJobs = async (req, res) => {
+  res.status(200).json({ jobs });
+};
+
+export const createJob = async (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  }
+  const id = uuidv4(10);
+
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(201).json({ job });
+};
+
+export const getJob = async (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    throw new Error("NO job with that id");
+    return res.status(404).json({ msg: `NO Job with id ${id}` });
+  }
+  res.status(200).json({ job });
+};
+
+export const editJob = async (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "Please enter company and position" });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `No job with id ${id}` });
+  }
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: "job modified", job });
+};
+
+export const deleteJob = async (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+
+  res.status(200).json({ msg: "job deleted" });
+};
--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const app = express();
 const morgan = require("morgan");
 //nanoid is giving error
 
-const { v4: uuidv4 } = require("uuid");
-let jobs = [
-  { id: uuidv4(), company: "apple", position: "front-end" },
-  { id: uuidv4(), company: "google", position: "back-end" },
-];
-
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -29,63 +23,25 @@ app.post("/", (req, res) => {
 });
 
 //GET ALL JOBS
-app.get("/api/v1/jobs", (req, res) => {
-  res.status(200).json({ jobs });
-});
+app.get("/api/v1/jobs");
 
 //CREATE JOBS
-app.post("/api/v1/jobs", (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "please provide company and position" });
-  }
-  const id = uuidv4(10);
-
-  const job = { id, company, position };
-  jobs.push(job);
-  res.status(201).json({ job });
-});
+app.post("/api/v1/jobs");
 
 //GET SINGLE JOB
-app.get("/api/v1/jobs/:id", (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: `NO Job with id ${id}` });
-  }
-  res.status(200).json({ job });
-});
+app.get("/api/v1/jobs/:id");
 
 //EDIT JOBS
-app.patch("/api/v1/jobs/:id", (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "Please enter company and position" });
-  }
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: `No job with id ${id}` });
-  }
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ msg: "job modified", job });
-});
+app.patch("/api/v1/jobs/:id");
 
 //DELETE JOB
-app.delete("/api/v1/jobs/:id", (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
-
-  res.status(200).json({ msg: "job deleted" });
-});
+app.delete("/api/v1/jobs/:id");
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "NOT FOUND" });
+});
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "Something Went Wrong" });
 });
 const port = process.env.PORT || 5173;
 
