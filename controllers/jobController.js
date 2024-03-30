27777c58a3956b3e94f169cdfa665eda94bc428f
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-03-30 21:04:47 +0530

    mongo db

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -10,13 +10,7 @@ const getAllJobs = async (req, res) => {
 
 const createJob = async (req, res) => {
   const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "please provide company and position" });
-  }
-  const id = uuidv4(10);
-
-  const job = { id, company, position };
-  jobs.push(job);
+  const job = await Job.create({ company, position });
   res.status(201).json({ job });
 };
 
