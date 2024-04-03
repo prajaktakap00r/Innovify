f14d8d10a7eb0a1b517b3ad068e93368c9860ff9
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-04-03 18:22:25 +0530

    customErrors

--- errors/customError.js
+++ errors/customError.js
@@ -6,3 +6,17 @@ export class NotFoundError extends Error {
     this.statusCode = StatusCodes.NOT_FOUND;
   }
 }
+export class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "BadRequestError";
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+export class UnauthorizedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "UnauthenicatedError ";
+    this.statusCode = StatusCodes.FORBIDDEN;
+  }
+}
