c85dbcc7d09b529459b9b083a4102cc3c978732e
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-04-08 17:41:41 +0530

    password validate

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -3,6 +3,7 @@ import { BadRequestError, NotFoundError } from "../errors/customError.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
 import mongoose from "mongoose";
 import Job from "../models/JobModels.js";
+import User from "../models/UserModel.js";
 
 const withValidationErrors = (validateValues) => {
   return [
@@ -43,7 +44,20 @@ export const validateIdParam = withValidationErrors([
 
 export const validateInputRegister = withValidationErrors([
   body("name").notEmpty().withMessage("name is required"),
-  body("email").notEmpty().withMessage("email is required"),
+  body("email")
+    .notEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .custom(async (email) => {
+      const user = await User.findOne({ email });
+      if (user) {
+        throw new BadRequestError("email already exist");
+      }
+    }),
   body("location").notEmpty().withMessage("location is required"),
-  body("password").notEmpty().withMessage("password is required"),
+  body("password")
+    .notEmpty()
+    .withMessage("password is required")
+    .isLength({ min: 8, max: 20 })
+    .withMessage("password must be 8 characters long"),
 ]);
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { register, login } from "../controllers/authController.js";
+import { validateInputRegister } from "../middleware/validationMiddleware.js";
 const router = Router();
 
-router.post("/register", register);
+router.post("/register", validateInputRegister, register);
 router.post("/login", login);
 export default router;
