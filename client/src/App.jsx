5876d1c6ebc80a0207f05f3b274a5bd62f500fd8
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-04-14 11:28:58 +0530

    login backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ import {
 } from "../src/pages";
 
 import { action as registerAction } from "./pages/Register";
+import { action as loginAction } from "./pages/Login";
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
   document.body.classList.toggle("dark-theme", isDarkTheme);
@@ -42,6 +43,7 @@ const router = createBrowserRouter([
       {
         path: "login",
         element: <Login />,
+        action: loginAction,
       },
       {
         path: "DashBoard",
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,26 @@
+import { Link, Form, redirect, useNavigation } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
-import { Link } from "react-router-dom";
-import Logo from "../components/logo";
-import { FormRow } from "../components";
-
+import { FormRow, Logo } from "../components";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post("/auth/login", data);
+    toast.success("Login Successfully");
+    return redirect("/dashboard");
+  } catch (error) {
+    toast.error("error?.response?.data?.msg");
+    return error;
+  }
+};
 export default function Login() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
     <Wrapper>
-      <form className="form">
+      <Form className="form">
         <Logo />
         <h4>Login</h4>
         <FormRow
@@ -15,8 +29,8 @@ export default function Login() {
           defaultValue="kprajakta235@gmail.com"
         />
         <FormRow type="password" name="Password" defaultValue="secret123" />
-        <button className="btn btn-block" type="submit">
-          Submit
+        <button className="btn btn-block" type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Submit"}
         </button>
         <button className="btn btn-block">Explore the App</button>
         <p>
@@ -26,7 +40,7 @@ export default function Login() {
             Register
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 }
