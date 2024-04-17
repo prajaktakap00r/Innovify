3f071db42ead94e0f05ef4b564b4a7a403862be5
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-04-17 17:56:16 +0530

    job status

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,6 @@
 export { default as Logo } from "./logo";
 export { default as FormRow } from "./FormRow";
-
+export { default as FromRowSelect } from "./FormRowSelect";
 export { default as SmallSidebar } from "./SmallSidebar";
 export { default as Navbar } from "./Navbar";
 export { default as BigSideBar } from "./BigSideBar";
