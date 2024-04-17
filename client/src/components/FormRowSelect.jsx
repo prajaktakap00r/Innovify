87c8a942767f3724bd00e1b94c358e7501709d65
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-04-17 17:51:46 +0530

    formrow

---
+++ client/src/components/FormRowSelect.jsx
@@ -0,0 +1,28 @@
+export default function FromRowSelect({
+  name,
+  labelText,
+  list,
+  defaultValue = "",
+}) {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+      <select
+        name={name}
+        id={name}
+        className="form-select"
+        defaultValue={defaultValue}
+      >
+        {list.map((itemValue) => {
+          return (
+            <option key={itemValue} value={itemValue}>
+              {itemValue}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+}
