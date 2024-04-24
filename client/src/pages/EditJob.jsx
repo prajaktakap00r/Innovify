974b6ff677a82487f575064a57b84871b3c825fe
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-04-24 23:43:06 +0530

    edit job functionality

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,3 +1,17 @@
+import { FormRow, FormRowSelect } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useLoaderData } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
+
+export const loader = async () => {
+  return null;
+};
+export const action = async () => {
+  return null;
+};
 export default function EditJob() {
   return (
     <div>
