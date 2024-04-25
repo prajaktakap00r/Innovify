a953f2b1f72d14e8369414043bac888ad3a041cd
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-04-25 16:57:08 +0530

    admin page setup done

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,3 +6,4 @@ export { default as Navbar } from "./Navbar";
 export { default as BigSideBar } from "./BigSideBar";
 export { default as JobsContainer } from "./JobsContainer";
 export { default as SearchContainer } from "./SearchContainer";
+export { default as StatItem } from "./StatItem";
--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,8 +1,11 @@
 import { FaSuitcaseRolling, FaCalendarCheck } from "react-icons/fa";
+import { CiUser } from "react-icons/ci";
+import { PiBag } from "react-icons/pi";
 import { useLoaderData, redirect } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import Wrapper from "../assets/wrappers/StatsContainer";
 import { toast } from "react-toastify";
+import { StatItem } from "../components";
 
 export const loader = async () => {
   try {
@@ -17,7 +20,20 @@ export default function Admin() {
   const { users, jobs } = useLoaderData();
   return (
     <Wrapper>
-      <h1>Admin Layout</h1>
+      <StatItem
+        title="current users"
+        count={users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<CiUser />}
+      />
+      <StatItem
+        title="current jobs"
+        count={jobs}
+        color="#647acb"
+        bcg="#e0e8f9"
+        icon={<PiBag />}
+      />
     </Wrapper>
   );
 }
