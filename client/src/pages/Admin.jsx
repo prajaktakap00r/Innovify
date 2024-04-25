cb1483937cc66e48b2749609fa41e027fe7dfe35
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-04-25 16:23:24 +0530

    admin page setup

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,3 +1,8 @@
+import { FaSuitcaseRolling, FaCalendarCheck } from "react-icons/fa";
+import { useLoaderData, redirect } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import Wrapper from "../assets/wrappers/StatsContainer";
+import { toast } from "react-toastify";
 export default function Admin() {
   return (
     <div>
