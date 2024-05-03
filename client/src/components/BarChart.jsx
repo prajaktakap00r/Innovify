45ebae024ba8df0bd9859c944b33f753af27d3c0
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-05-03 09:56:54 +0530

    bar chart and area chart added

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -1,3 +1,22 @@
-export default function BarChart() {
-  return <h1>Bar Chart</h1>;
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+export default function BarChartComponent({ data }) {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date " />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" barSize={75} fill="#eb5e28" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 }
--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,3 +1,21 @@
-export default function ChartsConatiner() {
-  return <h1>Chart Container</h1>;
+import { useState } from "react";
+
+import Wrapper from "../assets/wrappers/ChartsContainer";
+import AreaChartComponent from "./AreaChart";
+import BarChartComponent from "./BarChart";
+export default function ChartsConatiner({ data }) {
+  const [barChart, setBarChart] = useState(true);
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "AreaChart" : "BarChart"}
+      </button>
+      {barChart ? (
+        <BarChartComponent data={data} />
+      ) : (
+        <AreaChartComponent data={data} />
+      )}
+    </Wrapper>
+  );
 }
