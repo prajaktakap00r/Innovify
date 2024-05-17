b349717b775f5c9f3f7bd33f973a06022eb3d3e5
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-05-17 13:20:08 +0530

    Search job setup

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,3 +1,9 @@
+import { FormRow, FromRowSelect } from ".";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { Form, useSubmit, Link } from "react-router-dom";
+import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from "../../../utils/constants";
+import { useAllJobsContext } from "../pages/AllJobs";
+
 export default function SearchContainer() {
   return <h2>Search Container</h2>;
 }
